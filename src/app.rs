7939ed09d0dbbf3985f4dd2f3d258e64e4deb9aa
etash <etashdoteth@gmail.com>
2022-10-19 15:51:51 -0400

    ♻️ heavy refactoring of app logic

---
+++ src/app.rs
@@ -0,0 +1,43 @@
+use clap::{crate_description, crate_name, crate_version, AppSettings, Arg, Command};
+
+pub fn build_app() -> Command<'static> {
+    Command::new(crate_name!())
+        .version(crate_version!())
+        .about(crate_description!())
+        .color(clap::ColorChoice::Auto)
+        .dont_collapse_args_in_usage(true)
+        .subcommand_required(true)
+        .arg_required_else_help(true)
+        .allow_negative_numbers(false)
+        .subcommand(
+            Command::new("generate")
+                .alias("gen")
+                .alias("create")
+                .about("Generate asset(s) based on given input layers")
+                .arg(
+                    Arg::new("number")
+                        .help("Number of assets to generate in the current run")
+                        .takes_value(true)
+                        .default_value("5")
+                        .value_name("count"),
+                )
+                .arg(
+                    Arg::new("unique")
+                        .long("unique")
+                        .short('u')
+                        .help(
+                            "Boolean flag to indicate whether the generated set \
+                                should be all unique assets",
+                        )
+                        .takes_value(true)
+                        .possible_values(&["yes", "y", "no", "n"])
+                        .value_name("flag")
+                        .default_value("yes"),
+                ),
+        )
+}
+
+#[test]
+fn verify_app() {
+    build_app().debug_assert()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,71 @@
 use console::style;
-use std::path::Path;
-use std::{env, process};
+// use std::path::Path;
+// use std::{env, process};
 
-use artgen_rs::{
-    constants::PALETTE_EMOJI,
-    run::{run, Config},
+use artgen::{
+    app::build_app,
+    constants::*,
+    // run::{run, Config},
     utils::*,
 };
 
+use anyhow;
+
+type ExitCode = i32;
+
 fn main() {
-    // println!(
-    //     "\n{} {}\n",
-    //     style("Time to overhaul the codebase!").cyan().bold(),
-    //     PALETTE_EMOJI
-    // );
+    println!(
+        "\n{} {}\n",
+        style("Time to overhaul the codebase!").cyan().bold(),
+        PALETTE_EMOJI
+    );
+
+    let result = run();
+    match result {
+        Ok(exit_code) => {
+            std::process::exit(exit_code);
+        }
+        // TODO: Provide better error reporting
+        Err(err) => {
+            eprintln!(
+                "\n{} {} {}\n",
+                style("There was an error during the run:").red().bold(),
+                err,
+                ERROR_EMOJI
+            );
+            std::process::exit(1);
+        }
+    }
+
+    // let config = Config::new(env::args()).unwrap_or_else(|err| {
+    //     eprintln!("Problem parsing arguments: {}", err);
+    //     process::exit(1);
+    // });
+    // if let Err(e) = run(Path::new(&config.dir)) {
+    //     eprintln!("Application error: {}", e);
+    //     process::exit(1);
+    // }
+    // TODO: Error handling if asset folders don't follow required convention
+    // TODO: Error handling for .DS_STORE files
+    // TODO: Update piece of metadata without regenerating assets
+    // TODO: Wipe assets and regenerate everything
+    // TODO: Add Config
+}
+
+fn run() -> anyhow::Result<ExitCode> {
+    let app = build_app();
+    let matches = app.get_matches();
+
     println!(
         "\n{} {}\n",
         style("We're generating some digital art!").yellow().bold(),
         PALETTE_EMOJI
     );
 
-    let config = Config::new(env::args()).unwrap_or_else(|err| {
-        eprintln!("Problem parsing arguments: {}", err);
-        process::exit(1);
-    });
-    if let Err(e) = run(Path::new(&config.dir)) {
-        eprintln!("Application error: {}", e);
-        process::exit(1);
+    if let Some((subcommand, matches)) = matches.subcommand() {
+    } else {
+        unreachable!("Subcommand is required");
     }
-    // // TODO: Error handling if asset folders don't follow required convention
-    // // TODO: Error handling for .DS_STORE files
-    // // TODO: Update piece of metadata without regenerating assets
-    // // TODO: Wipe assets and regenerate everything
+
+    Ok(0)
 }
